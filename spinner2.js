f0cbce3b31dcc0025d9c2928226f85b815960a72
alexannmill <alexannmill@gmail.com>
2022-08-30 21:50:44 +0000

    adding complete spinner2 refc

---
+++ spinner2.js
@@ -0,0 +1,54 @@
+
+let interval = 0
+let spinner = ['| ', '/', '-', '\\', '|', '/', '-', '\\']
+
+for (const rot of spinner) {
+  setTimeout(() => {
+    process.stdout.write(`\r ${rot}`)
+    // console.log(rot)
+  }, interval);
+  interval += 200
+}
+
+setTimeout(() => {
+  process.stdout.write("\n");
+}, interval);
+
+// setTimeout(() => {
+//   process.stdout.write('\r| ')
+// }, 100);
+
+// setTimeout(() => {
+//   process.stdout.write('\r/ ')
+// }, 300);
+
+// setTimeout(() => {
+//   process.stdout.write('\r- ')
+// }, 500);
+
+// setTimeout(() => {
+//   process.stdout.write('\r\\ ')
+// }, 700);
+
+// setTimeout(() => {
+//   process.stdout.write('\r| ')
+// }, 900);
+
+// setTimeout(() => {
+//   process.stdout.write('\r/ ')
+// }, 1100);
+
+// setTimeout(() => {
+//   process.stdout.write('\r- ')
+// }, 1300);
+
+// setTimeout(() => {
+//   process.stdout.write('\r\\ ')
+// }, 1500);
+
+// setTimeout(() => {
+//   process.stdout.write('\r|  ')
+// }, 1700);
+
+
+
